80896b41327b7983f501aee3525bb2915f042f93
Olena Stepanova <86sthelen@gmail.com>
2019-04-24 08:42:00 -0300

    ex1

--- ArrayListSimpleEx/Form1.cs
+++ ArrayListSimpleEx/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace ArrayListSimpleEx
 {
@@ -15,6 +16,69 @@ namespace ArrayListSimpleEx
         public Form1()
         {
             InitializeComponent();
+        }
+        private ArrayList message = new ArrayList();
+        bool isReversed = false;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            message.Add("I");
+            message.Add("Love");
+            message.Add("Programming");
+            message.Add("So");
+            message.Add("Much");
+        
+        }
+
+        private void btnShowMsg_Click(object sender, EventArgs e)
+        {
+            
+            if (isReversed)
+            {
+                message.Reverse();
+            }
+            lblMessage.Text = ComposeMessage(message);
+            isReversed = false;
+        }
+
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            message.Reverse();
+            lblMessage.Text = ComposeMessage(message);
+            isReversed = true;
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (isReversed)
+            {
+                MessageBox.Show("You cannot add word in reverse order. Click Display button.");
+            }
+            else
+            {
+                string userWord = txtSecondPos.Text;
+                message.Insert(1, userWord);
+                
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            message.RemoveAt(1);
+        }
+
+        private string ComposeMessage(ArrayList list)
+        {
+            string messageDisplay = "";
+            foreach (string word in list)
+            {
+                messageDisplay += $"{word.ToString()} ";
+            }
+
+            return messageDisplay;
         }
     }
 }
